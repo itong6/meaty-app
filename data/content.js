ebd523d01dd2233a082016524b7950a50083a404
itong6 <itong6@my.bcit.ca>
2022-04-24 18:18:06 -0700

    cow path

--- data/content.js
+++ data/content.js
@@ -14,7 +14,8 @@ export const subheading = [
     "Click the knife to slit her throat",
     "Defeather Claudia by clicking her feathers",
     "Click on the cleaver until Claudia is decapitated",
-    "Disembowel Claudia by clicking on the organs"
+    "Disembowel Claudia by clicking on the organs",
+    "Click the target on Nellie's head."
 ]
 
 export const button = [
@@ -32,4 +33,13 @@ export const chicken_indicator = [
     "../indicator/ch/indicator3.svg",
     "../indicator/ch/indicator4.svg",
     "../indicator/ch/indicator5.svg",
+]
+
+export const indicator = [
+    "../indicator/cp/ indicator0.svg",
+    "../indicator/cp/indicator1.svg",
+    "../indicator/cp/indicator2.svg",
+    "../indicator/cp/indicator3.svg",
+    "../indicator/cp/indicator4.svg",
+
 ]
